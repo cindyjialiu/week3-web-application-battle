45faed17a5f9d68ad15cf26c0c44eedbc6c6b2bf
Cindy <jl7e12@gmail.com>
2018-03-08 00:31:00 +0000

    Added view HP and test passed

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -4,12 +4,20 @@ feature 'Entering names' do
   #   expect(page).to have_content 'Testing infrastructure is working!'
   # end
 
-  scenario 'Can fill in user names and print on-screen' do
+  scenario 'Entering user names and print on-screen' do
     visit('/')
     fill_in :name1, with: "name1"
     fill_in :name2, with: "name2"
     click_button "Submit"
     expect(page).to have_content 'name1 vs. name2'
+  end
+
+  scenario 'Can see my progress bar and hit points of player2' do
+    visit('/')
+    fill_in :name1, with: "Shaun"
+    fill_in :name2, with: "Flossie"
+    click_button "Submit"
+    expect(page).to have_content 'Flossie: 60HP'
   end
 
 end
