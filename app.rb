4badfff89360d1061cdff7a0fddf73d091fbc060
Cindy <jl7e12@gmail.com>
2018-03-09 17:33:38 +0000

    Refactoring

--- app.rb
+++ app.rb
@@ -8,20 +8,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $name1 = Player.new(params[:name1])
-    $name2 = Player.new(params[:name2])
+    $name1 = Player.new(params[:player_name1])
+    $name2 = Player.new(params[:player_name2])
     redirect to('/play')
   end
 
   get '/play' do
-    @name1 = $name1.name
-    @name2 = $name2.name
+    @player_name1 = $name1.name
+    @player_name2 = $name2.name
     erb(:play)
   end
 
   get '/attack' do
-    @name1 = $name1.name
-    @name2 = $name2.name
+    @player_name1 = $name1.name
+    @player_name2 = $name2.name
     erb(:attack)
   end
 
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
   visit('/')
-  fill_in :name1, with: "Shaun"
-  fill_in :name2, with: "Flossie"
+  fill_in :player_name1, with: "Shaun"
+  fill_in :player_name2, with: "Flossie"
   click_button "Submit"
 end
